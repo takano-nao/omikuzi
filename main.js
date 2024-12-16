734d60654cbad855df60223da38135b1dda1038d
takano-nao <takanaooo5@gmail.com>
2024-12-16 21:05:58 +0900

    Ref リファクタリング

--- main.js
+++ main.js
@@ -2,13 +2,20 @@
 
 {
   const btn = document.getElementById('btn');
-  const result =document.getElementById('result');
+  const result = document.getElementById('result');
 
- btn.addEventListener('click',()=>{
-   const results = ['大吉','中吉','凶','小吉'];
-   const n = Math.floor(Math.random() * results.length);
+  btn.addEventListener('click', () => {
+    const omikujiResult = getOmikujiResult();
+    displayResult(omikujiResult);
+  });
 
-   result.textContent = results[n];
+  function getOmikujiResult() {
+    const results = ['大吉', '中吉', '凶', '小吉'];
+    const randomIndex = Math.floor(Math.random() * results.length);
+    return results[randomIndex];
+  }
 
- });
+  function displayResult(result) {
+    result.textContent = result;
+  }
 }

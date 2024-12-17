17410ae21be38930e4c22e3cdbfcf3123dd93b92
takano-nao <takanaooo5@gmail.com>
2024-12-17 23:05:27 +0900

    update 運勢の割合を指定

--- main.js
+++ main.js
@@ -12,14 +12,20 @@
 
   function getOmikujiResult() {
     const results = [
-      { fortune: '大吉', message: '今日は最高の日です！' },
-      { fortune: '中吉', message: '今日は良い日です！' },
-      { fortune: '吉', message: '今日はまあまいい日です。' },
-      { fortune: '凶', message: '今日はいろいろ気を付けましょう。' },
-      { fortune: '大凶', message: '今日はひたすら耐えましょう…。' }
+      { fortune: '大吉', message: '今日は最高の日です！', probability: 0.1 },
+      { fortune: '中吉', message: '今日は良い日です！', probability: 0.2 },
+      { fortune: '吉', message: '今日はまあまいい日です。', probability: 0.3 },
+      { fortune: '凶', message: '今日はいろいろ気を付けましょう。', probability: 0.3 },
+      { fortune: '大凶', message: '今日はひたすら耐えましょう…。', probability: 0.1 }
     ];
-    const randomIndex = Math.floor(Math.random() * results.length);
-    return results[randomIndex];
+    const random = Math.random();
+    let cumulativeProbability = 0;
+    for (const result of results) {
+      cumulativeProbability += result.probability;
+      if (random < cumulativeProbability) {
+        return result;
+      }
+    }
   }
 
   function displayResult(omikujiResult) {
